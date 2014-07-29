8d6973a342164592ea2a56b651aaaa4e03909620
jasoncho0129 <jasoncho0129@hotmail.com>
2014-07-28 20:58:16 -0700

    adding extension bar popup

--- extension/extension.js
+++ extension/extension.js
@@ -1,4 +1,10 @@
-function savepopup(info, tab){
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+  window.open('http://cse134-135-2014.github.io/cse134_group10/main.html', '_blank');
+}
+
+
+function savePopup(info, tab){
   var url = 'http://cse134-135-2014.github.io/cse134_group10/extension/savepopup.html#urlInput=';
   //alert("info: " + info.srcUrl);
   //alert("url: " + url);
@@ -7,9 +13,21 @@ function savepopup(info, tab){
   //alert("tab: " + JSON.stringify(tab));
 }
 
+// Create one test item for each context type.
+var contexts = ["page","selection","link","editable","image","video",
+                "audio"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Go to Meme Master page";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  //console.log("'" + context + "' item:" + id);
+}
+
+
 var title = "Add this image to Meme Master";
 chrome.contextMenus.create({"title": title, "contexts":["image"],
-                            "onclick": savepopup});
+                            "onclick": savePopup});
 
 console.log("About to try creating an invalid item - an error about " +
             "item 999 should show up");
